4e202519a9a1dc6ef1e92ed552eeab887a7ccd3c
Faraz-Gul23 <Farazguljamali@gmail.com>
2025-10-06 01:56:22 -0700

    Update script.js

--- Task2_Frontend/script.js
+++ Task2_Frontend/script.js
@@ -37,7 +37,7 @@ const profiles = [
     },
     {
         id: 6,
-        name: "Abid ",
+        name: "Alina ",
         role: "Data Scientist",
         description: "Extracting insights from complex datasets using machine learning and statistical analysis. Proficient in Python and R.",
         image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
